0f4a3b729e598c7358536e59c99cb89a48e6972a
parispiggy <125295204+parispiggy@users.noreply.github.com>
2023-09-10 11:10:06 +0000

    Release 2.0.4

--- Workshop_facilitation/generate-groups.py
+++ Workshop_facilitation/generate-groups.py
@@ -4,4 +4,19 @@ from botocore.exceptions import ClientError
 from pathlib import Path
 import os
 import json
-import 
+import argparse
+import shutil
+import pandas as pd
+
+TMP_FOLDER = 'LINK_TEMP'
+IP_DICT_JSON_NAME = 'active_ips.json'
+GROUP_DICT_NAME = 'groups.json'
+GROUP_EXCEL_NAME = 'Groups.xlsx'
+AMI_ID = 'ami-060e29600c7769bc0'
+
+
+def bucket_exists(bucket_name):
+    """Determine whether bucket_name exists and the user has permission to access it
+
+    :param bucket_name: string
+    :return: True if 
